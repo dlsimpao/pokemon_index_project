9ccef89d28b213694b24159f952bbe30fa719bde
dlsimpao <dlsimpao@yahoo.com>
2020-06-09 17:52:06 -0700

    library(packages) instead of lapply for shiny io

--- global.R
+++ global.R
@@ -1,17 +1,35 @@
 # packages
+library(shiny)
+library(shinythemes)
+library(shinycssloaders)
+library(shinyWidgets)
+library(tidyverse)
+library(knitr)
+library(ggfortify)
+library(plotly)
+library(FNN)
+library(jsonlite)
+library(lubridate)
+library(httr)
+library(rvest)
+library(shinyjs)
+library(gtrendsR)
+
+#To install
 packages <- c(
-  "shiny", "shinythemes", "shinycssloaders", "shinyWidgets", 
-  "tidyverse", "knitr", "ggfortify", "plotly", "FNN", "jsonlite", "lubridate", "httr",
-  "rvest", "shinyjs", "gtrendsR"
+  "shiny", "shinythemes", "shinycssloaders","shinyWidgets", "tidyverse", "knitr", "ggfortify",
+  "plotly", "FNN", "jsonlite", "lubridate", "httr","rvest", "shinyjs", "gtrendsR"
 )
 
 # Inspired by https://github.com/ThiagoValentimMarques/The-ten-most-similar-players-Pro-Evolution-Soccer-2019
 
-package.check <- lapply(packages, FUN = function(x) {
-  if (!require(x, character.only = TRUE)) {
-    install.packages(x, dependencies = TRUE)
-  }
-})
+
+
+# package.check <- lapply(packages, FUN = function(x) {
+#   if (!require(x, character.only = TRUE)) {
+#     install.packages(x, dependencies = TRUE)
+#   }
+# })
 
 
 
