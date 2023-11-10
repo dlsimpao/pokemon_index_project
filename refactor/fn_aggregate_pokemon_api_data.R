e02e39d1f5c8f35b2bf2738fb768852a630ec084
dlsimpao <rlsimpao@ucdavis.edu>
2023-11-10 11:36:41 -0800

    added sample pokedex aggregator level 2

--- refactor/fn_aggregate_pokemon_api_data.R
+++ refactor/fn_aggregate_pokemon_api_data.R
@@ -12,18 +12,75 @@ jsl <- loadNamespace("jsonlite")
 # unused # https://pokeapi.co/api/v2/pokemon/{id or name}/encounters
 
 # functions
-get_uri_list = function(uri, limit, outfile){
-  response = httr::GET(uri, query = list(limit=limit, offset=0))
-  print(response)
-  data = content(response, as = "text") %>% jsl$fromJSON()
-  
-  pk_df = as.data.frame(data$results)
-  
-  # write data frame to csv
-  write.csv(pk_df, file=paste0("data/raw/api_",outfile,".csv"), row.names=FALSE)
+get_uri_list = function(uri, limit, outfile) {
+  result <- tryCatch({
+    response = httr::GET(uri, query = list(limit = limit, offset = 0))
+    data = content(response, as = "text") %>% jsl$fromJSON()
+    
+    pk_df = as.data.frame(data$results)
+    
+    # write data frame to csv
+    write.csv(pk_df,
+              file = paste0("data/raw/api_", outfile, ".csv"),
+              row.names = FALSE)
+    message(paste0("Success handling ", uri))
+  }, error = function(e) {
+    message(paste0("Error handling uri ", uri, "\n", e$message))
+    return(NA)
+  })
 }
 
-moveapi_config <- list(
+# vector of api request configurations
+berries_config <- list(
+  bry=list(uri="https://pokeapi.co/api/v2/berry", limit=100, outfile="berries"),
+  frm=list(uri="https://pokeapi.co/api/v2/berry-firmness", limit=100, outfile="berry_firmness"),
+  fvr=list(uri="https://pokeapi.co/api/v2/berry-flavor", limit=100, outfile="berry_flavor")
+)
+
+contests_config <- list(
+  cnt=list(uri="https://pokeapi.co/api/v2/contest-type", limit=100, outfile="contest"),
+  eff=list(uri="https://pokeapi.co/api/v2/contest-effect", limit=100, outfile="contest_effect"),
+  sef=list(uri="https://pokeapi.co/api/v2/super-contest-effect", limit=100, outfile="contest_super_effect")
+)
+
+encounter_config <- list(
+  mtd=list(uri="https://pokeapi.co/api/v2/encounter-method", limit = 100, outfile="encounter_method"),
+  cnd=list(uri="https://pokeapi.co/api/v2/encounter-condition", limit = 100, outfile="encounter_condition"),
+  cnv=list(uri="https://pokeapi.co/api/v2/encounter-condition-value", limit = 100, outfile="encounter_cond_value")
+)
+
+evolution_config <- list(
+  chn=list(uri="https://pokeapi.co/api/v2/evolution-chain", limit=100, outfile="evolution_chain"),
+  trg=list(uri="https://pokeapi.co/api/v2/evolution-trigger", limit=100, outfile="evolution_trigger")
+)
+
+games_config <- list(
+  gen=list(uri="https://pokeapi.co/api/v2/generation", limit=100, outfile="games_generation"),
+  pok=list(uri="https://pokeapi.co/api/v2/pokedex", limit=100, outfile="games_pokedex"),
+  ver=list(uri="https://pokeapi.co/api/v2/version", limit=100, outfile="games_version"),
+  vgp=list(uri="https://pokeapi.co/api/v2/version-group", limit=100, outfile="games_version_group")
+)
+
+items_config <- list(
+  itm=list(uri="https://pokeapi.co/api/v2/item", limit=100, outfile="item"),
+  atr=list(uri="https://pokeapi.co/api/v2/item-attribute", limit=100, outfile="item_attribute"),
+  cat=list(uri="https://pokeapi.co/api/v2/item-category", limit=100, outfile="item_category"),
+  fli=list(uri="https://pokeapi.co/api/v2/item-fling-effect", limit=100, outfile="item_fling_effect"),
+  pkt=list(uri="https://pokeapi.co/api/v2/item-pocket", limit=100, outfile="item_pocket")
+)
+
+locations_config <- list(
+  loc=list(uri="https://pokeapi.co/api/v2/location", limit=100, outfile="location"),
+  lca=list(uri="https://pokeapi.co/api/v2/location-area", limit=100, outfile="location_area"),
+  ppa=list(uri="https://pokeapi.co/api/v2/pal-park-area", limit=100, outfile="location_pal_park_area"),
+  reg=list(uri="https://pokeapi.co/api/v2/region", limit=100, outfile="location_region")
+)
+
+machines_config <- list(
+  mac=list(uri="https://pokeapi.co/api/v2/machine", limit=1000, outfile="machine")
+)
+
+moves_config <- list(
   mov=list(uri="https://pokeapi.co/api/v2/move", limit=10000, outfile="moves"),
   ail=list(uri="https://pokeapi.co/api/v2/move-ailment", limit=10000, outfile="move_ailment"),
   bst=list(uri="https://pokeapi.co/api/v2/move-battle-style", limit=10000, outfile="move_battle_style"),
@@ -31,9 +88,8 @@ moveapi_config <- list(
   mdc=list(uri="https://pokeapi.co/api/v2/move-damage-class", limit=10000, outfile="move_damage_class"),
   mlm=list(uri="https://pokeapi.co/api/v2/move-learn-method", limit=10000, outfile="move_learn_method"),
   trg=list(uri="https://pokeapi.co/api/v2/move-target", limit=10000, outfile="move_target")
-  )
+)
 
-# vector of api request configurations
 pokeapi_config <- list(
   mon=list(uri="https://pokeapi.co/api/v2/pokemon", limit=10000, outfile="pokemon"),
   abl=list(uri="https://pokeapi.co/api/v2/ability", limit=1000, outfile="ability"),
@@ -52,15 +108,28 @@ pokeapi_config <- list(
   typ=list(uri="https://pokeapi.co/api/v2/type",limit=100, outfile="type")
 )
 
+# for granular execution
 # ! to filter use !
-pokeapi_config = pokeapi_config[c(1)]
+# pokeapi_config = pokeapi_config[c(1)]
 # 
-for(item in pokeapi_config){
-  get_uri_list(uri = item$uri, limit = item$limit, outfile=item$outfile)
-}
-
-# for (item in moveapi_config){
-#     get_uri_list(uri = item$uri, limit = item$limit, outfile=item$outfile)
+# for(item in pokeapi_config){
+#   get_uri_list(uri = item$uri, limit = item$limit, outfile=item$outfile)
 # }
+# 
 
-gc()
+
+super_config = c(berries_config,
+                 contests_config,
+                 encounter_config,
+                 evolution_config,
+                 games_config,
+                 items_config,
+                 locations_config,
+                 machines_config,
+                 moves_config,
+                 pokeapi_config)
+
+for(item in super_config){
+  get_uri_list(uri = item$uri, limit=item$limit, outfile=item$outfile)
+  gc()
+}
--- refactor/fn_aggregate_pokemon_api_data_level2.R
+++ refactor/fn_aggregate_pokemon_api_data_level2.R
@@ -6,10 +6,19 @@ library(tidyverse)
 library(jsonlite)
 library(odbc)
 
+stringify_list <- function(item_list) {
+  return(paste(item_list, collapse = ","))
+}
+
+# to do
+# get all pokemon's attributes
+# get pokemon shape and merge
+
 tidy <- loadNamespace("tidyverse")
 jsl <- loadNamespace("jsonlite")
-pokemon_uri_list = read_csv("data/raw/api_pokemon.csv", col_types = "c") %>% head(1)
+pokemon_uri_list = read_csv("data/raw/api_pokemon.csv", col_types = "c") %>% head(3)
 
+pokemon_shape_list = read_csv("data/raw/api_pokeshape.csv", col_types = "c") %>% head(1)
 # to consider comparing
 # abilities
 # id
@@ -19,22 +28,76 @@ pokemon_uri_list = read_csv("data/raw/api_pokemon.csv", col_types = "c") %>% hea
 # types
 # height
 # weight
+# shape
 
-print(pokemon_uri_list$url)
+# base_stat_name = "hp,attack,defense,special-attack,special-defense,speed",
+pokedex = data.frame(
+  id = character(0),
+  name = character(0),
+  type_list = character(0),
+  ability_list = character(0),
+  moveset = character(0),
+  stats = character(0),
+  base_stats = character(0),
+  height = character(0),
+  weight = character(0),
+  area_encounter = character(0)
+)
 
-for(mon in pokemon_uri_list$url){
-  response = httr::GET(mon)
-  data = content(response, as = "text") %>% jsl$fromJSON()
-  
-  # data to collect
-  print(data$abilities$ability$name)
-  print(data$past_abilities)
-  print(data$id)
-  print(data$location_area_encounters)
-  print(data$moves$move$name)
-  print(data$stats$base_stat)
-  print(data$stats$effort)
-  print(data$stats$stat$name)
-  print(data$types$type$name)
-  print(data$past_types)
+for (mon in pokemon_uri_list$url) {
+  tryCatch({
+    response = httr::GET(mon)
+    
+    sink(
+      "refactor/FN_aggregate_pokemon_api_level2_log.txt",
+      type = "output",
+      append = FALSE
+    )
+    message(paste0("CODE: ", response$status_code,
+                   " : ", response$url))
+    
+    data = content(response, as = "text") %>% jsl$fromJSON()
+    
+    to_process = list(
+      types = data$types$name,
+      abiltiies = data$abilities$ability$name,
+      moveset = data$moves$move$name,
+      stats = data$stats$base_stat,
+      base_stats = data$stats$effort
+    )
+    
+    processed = lapply(to_process, function(col)
+      stringify_list(col))
+    
+    new_record = data.frame(
+      id = data$id,
+      name = data$name,
+      type_list = processed$types,
+      ability_list = processed$abiltiies,
+      moveset = processed$moveset,
+      stats = processed$stats,
+      base_stats = processed$base_stats,
+      height = data$height,
+      weight = data$weight,
+      area_encounter = data$location_area_encounters
+    )
+    
+    pokedex = rbind(pokedex, new_record)
+    message(paste0("success: added ", data$name, " to pokedex"))
+    
+    gc()
+  }, error = function(e) {
+    message(paste0("ERROR processing ", mon))
+    message(e)
+  })
 }
+
+# for (mon in pokemon_uri_list$url) {
+#   tryCatch({
+#     response = httr::GET(mon)
+#   },error = function(e){
+#     message(e)
+#   }
+sink()
+
+print(head(pokedex))
